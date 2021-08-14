e288387e3ecb9d0c8909d6c631d60ce2aa565bfc
0-Faint-0 <vasyanadtochiy146@gmail.com>
2021-08-14 18:28:11 +0300

    Added script to change the order state

---
+++ changeOrderState.php
@@ -0,0 +1,15 @@
+<?php
+	include 'connection.php';
+
+	$new_state = $_POST['state'];
+	$id = $_POST['id'];
+
+	$sql = "UPDATE orders SET state = '".$new_state."' WHERE id = '".$id."'";
+
+	if (mysqli_query($conn, $sql)) {
+		echo "SUCCESS";
+	}
+	else {
+		echo "Error updating data: " . $conn->error;
+	}
+?>
