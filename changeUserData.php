df733ef8d3725a71dcd69f0ec1617dd6f8914f44
0-Faint-0 <vasyanadtochiy146@gmail.com>
2021-08-20 11:21:59 +0300

    Added the ability to change user phone

--- changeUserData.php
+++ changeUserData.php
@@ -6,14 +6,14 @@
 	$sec_data = 0;
 
 	if($_GET['apicall'] == 'username') {
-		$username = $_POST['username'];
+		$user_name = $_POST['username'];
 
-		$change_in_orders = "UPDATE orders SET order_name = '".$username."' WHERE order_phone = '".$phone."'";
+		$change_in_orders = "UPDATE orders SET order_name = '".$user_name."' WHERE order_phone = '".$phone."'";
 		if(mysqli_query($conn, $change_in_orders)) {
 			$sec_data = 1;
 		}
 
-		$change_data = "UPDATE users SET username = '".$username."' WHERE phone = '".$phone."'";
+		$change_data = "UPDATE users SET username = '".$user_name."' WHERE phone = '".$phone."'";
 	}
 	elseif($_GET['apicall'] == 'city') {
 		$city = $_POST['city'];
@@ -31,6 +31,26 @@
 		}
 
 		$change_data = "UPDATE users SET city = '".$city."' WHERE phone = '".$phone."'";
+	}
+	elseif($_GET['apicall'] == 'phone') {
+		$new_phone = $_POST['new_phone'];
+
+		$check_orders = $conn->prepare("SELECT id FROM orders WHERE order_phone = ? AND state < 3");
+		$check_orders->bind_param("s", $phone);
+		$check_orders ->execute();
+		$check_orders ->bind_result($id);
+
+		if($check_orders ->fetch()) {
+			$sec_data = 2;
+		}
+		else {
+			$change_in_orders = "UPDATE orders SET order_name = '".$username."' WHERE order_phone = '".$phone."'";
+			if(mysqli_query($conn, $change_in_orders)) {
+				$sec_data = 1;
+			}
+		}
+
+		$change_data = "UPDATE users SET phone = '".$new_phone."' WHERE phone = '".$phone."'";
 	}
 	else {
 		$sec_data = 1;
